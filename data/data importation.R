434ca39f50cff0c72831162ec0655f213e06b19c
vincentFlorquin <vflorquin@gmail.com>
2018-11-13 14:44:26 +0100

    data-changes-in-process

---
+++ data/data importation.R
@@ -0,0 +1,8 @@
+#Data importation
+SciViews::R
+(sleep <- read$csv("https://docs.google.com/spreadsheets/d/15bi2eKujJLabLjSyodTHFJKFzx1dk-GtS9fnImSb1-Y/export?format=csv", locale = locale(decimal_mark = ",")))
+
+write$rds(sleep, file = "./data/sleep", compress = "gz")
+
+sleep  <- read("./data/sleep.rds")
+
