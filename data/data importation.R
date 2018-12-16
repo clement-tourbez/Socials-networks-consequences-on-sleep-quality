37c8def75d41104299872b72e2820f0869a243c6
clement-tourbez <clement.tourbez@umons.ac.be>
2018-12-16 19:00:30 +0100

    folder structuration

--- data/data importation.R
+++ data/data importation.R
@@ -4,7 +4,22 @@
 SciViews::R
 (sleep <- read$csv("https://docs.google.com/spreadsheets/d/15bi2eKujJLabLjSyodTHFJKFzx1dk-GtS9fnImSb1-Y/export?format=csv", locale = locale(decimal_mark = ",")))
 
+
+
+
+
+
+
+
+
+sleep %>.%
+    filter(., ! is.na(age), ! is.na(bed_time)) -> sleep
+
+sleep$sex <- as.factor(sleep$sex)
+
+
+visdat::vis_dat(sleep)
+
 write$rds(sleep, file = "./data/sleep", compress = "gz")
 
-sleep  <- read("./data/sleep.rds")
-
+sleep  <- read("../data/sleep.rds")
