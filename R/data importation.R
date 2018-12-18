4ab9142d717797480063c0611d9e1c201e7e277e
clement-tourbez <clement.tourbez@umons.ac.be>
2018-12-18 21:14:27 +0100

    introducing distribution to rapport

--- R/data importation.R
+++ R/data importation.R
@@ -8,7 +8,7 @@ library("magrittr")
 (sleep <- read_csv("https://docs.google.com/spreadsheets/d/15bi2eKujJLabLjSyodTHFJKFzx1dk-GtS9fnImSb1-Y/export?format=csv", locale = locale(decimal_mark = ",")))
 
 
-#head(sleep)
+head(sleep)
 
 
 
@@ -16,6 +16,22 @@ sleep %>.%
     filter(., ! is.na(age), ! is.na(bed_time)) -> sleep
 
 sleep$sex <- as.factor(sleep$sex)
+
+
+## Cutting sleep$age into sleep$age_rec
+sleep$age_rec <- cut(sleep$age, include.lowest=TRUE,  right=TRUE,
+                     breaks=c(0, 30, 110))
+
+## Cutting sleep$video_game_time into sleep$video_game_time_rec
+sleep$vg_rec <- cut(sleep$video_game_time, include.lowest=TRUE,  right=TRUE,
+                                 breaks=c(0, 1, 2, 5, 20))
+
+## Cutting sleep$facebook_and_messenger_time into sleep$facebook_and_messenger_time_rec
+sleep$fb_rec <- cut(sleep$facebook_and_messenger_time, include.lowest=TRUE,  right=TRUE,
+                                             breaks=c(0, 1, 2, 5))
+## Cutting sleep$other_social_network_time into sleep$other_rec
+sleep$other_rec <- cut(sleep$other_social_network_time, include.lowest=TRUE,  right=TRUE,
+                       breaks=c(0, 1, 2, 5))
 
 
 visdat::vis_dat(sleep)
